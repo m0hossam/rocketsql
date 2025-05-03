3d06ad031b222d227b8f135bde4cc1bc2cc0fb67
m0hossam <mohossam1326@gmail.com>
2025-05-04 01:36:26 +0300

    Implement DDL/DML parsing

--- compiler/parser/parser.go
+++ compiler/parser/parser.go
@@ -1,6 +1,17 @@
 package parser
 
-import "errors"
+import (
+	"errors"
+)
+
+/*
+
+#################################################################
+# TODO: Remove unnecessary matches                              #
+# TODO: Refactor structs and functions into query, dml, and ddl #
+#################################################################
+
+*/
 
 /* Grammar:
 <Field> := IdTok
@@ -9,25 +20,26 @@ import "errors"
 <Term> := <Expression> = <Expression>
 <Predicate> := <Term> [ AND <Predicate> ]
 
-<Query> := SELECT <SelectList> FROM <TableList> [ WHERE <Predicate> ]
-<SelectList> := <Field> [ , <SelectList> ]
-<TableList> := IdTok [ , <TableList> ]
-
-<UpdateCmd> := <Insert> | <Delete> | <Modify> | <Create>
-<Create> := <CreateTable> | <CreateView> | <CreateIndex>
-
-<Insert> := INSERT INTO IdTok ( <FieldList> ) VALUES ( <ConstList> )
 <FieldList> := <Field> [ , <FieldList> ]
 <ConstList> := <Constant> [ , <ConstList> ]
 
+<Query> := SELECT <FieldList> FROM <FieldList> [ WHERE <Predicate> ]
+
+<DML> := <Insert> | <Delete> | <Update>
+
+<Insert> := INSERT INTO IdTok ( <FieldList> ) VALUES ( <ConstList> )
+
 <Delete> := DELETE FROM IdTok [ WHERE <Predicate> ]
 
-<Modify> := UPDATE IdTok SET <Field> = <Expression> [ WHERE <Predicate> ]
+<Update> := UPDATE IdTok SET <Field> = <Expression> [ WHERE <Predicate> ]
+
+<DDL> := <CreateTable>
 
 <CreateTable> := CREATE TABLE IdTok ( <FieldDefs> )
+
 <FieldDefs> := <FieldDef> [ , <FieldDefs> ]
 <FieldDef> := IdTok <TypeDef>
-<TypeDef> := INT | BIGINT | SMALLINT | FLOAT | DOUBLE | CHAR ( IntTok ) | VARCHAR ( IntTok )
+<TypeDef> := SMALLINT | INT | BIGINT | FLOAT | DOUBLE | CHAR ( IntTok ) | VARCHAR ( IntTok )
 */
 
 // <Field> := IdTok
@@ -45,6 +57,7 @@ type Constant struct {
 
 // <Expression> := <Field> | <Constant>
 type Expression struct {
+	IsField  bool
 	Field    *Field
 	Constant *Constant
 }
@@ -68,11 +81,79 @@ type FieldList struct {
 	Fields []*Field
 }
 
+// <ConstList> := <Constant> [ , <ConstList> ]
+type ConstList struct {
+	Constants []*Constant
+}
+
 // <Query> := SELECT <SelectList> FROM <TableList> [ WHERE <Predicate> ]
 type Query struct {
 	SelectList *FieldList
 	TableList  *FieldList
 	Predicate  *Predicate
+}
+
+// <TypeDef> := SMALLINT | INT | BIGINT | FLOAT | DOUBLE | CHAR ( IntTok ) | VARCHAR ( IntTok )
+type TypeDef struct {
+	Type string
+	Size int // Size for CHAR and VARCHAR types
+}
+
+// <FieldDef> := IdTok <TypeDef>
+type FieldDef struct {
+	Name    string
+	TypeDef *TypeDef
+}
+
+// <FieldDefs> := <FieldDef> [ , <FieldDefs> ]
+type FieldDefs struct {
+	FieldDefs []*FieldDef
+}
+
+// <CreateTable> := CREATE TABLE IdTok ( <FieldDefs> )
+type CreateTableData struct {
+	TableName string
+	FieldDefs *FieldDefs
+}
+
+// <Insert> := INSERT INTO IdTok ( <FieldList> ) VALUES ( <ConstList> )
+type InsertData struct {
+	TableName string
+	Fields    *FieldList
+	Values    *ConstList
+}
+
+// <Delete> := DELETE FROM IdTok [ WHERE <Predicate> ]
+type DeleteData struct {
+	TableName string
+	Predicate *Predicate
+}
+
+// <Update> := UPDATE IdTok SET <Field> = <Expression> [ WHERE <Predicate> ]
+type UpdateData struct {
+	TableName  string
+	Field      *Field
+	Expression *Expression
+	Predicate  *Predicate
+}
+
+type ParseTreeType int
+
+const (
+	SelectTree ParseTreeType = iota
+	InsertTree
+	DeleteTree
+	UpdateTree
+	CreateTableTree
+)
+
+type ParseTree struct {
+	Type            ParseTreeType
+	Query           *Query
+	CreateTableData *CreateTableData
+	InsertData      *InsertData
+	DeleteData      *DeleteData
+	UpdateData      *UpdateData
 }
 
 type Parser struct {
@@ -125,15 +206,57 @@ func (p *Parser) parseConstant() (*Constant, error) {
 func (p *Parser) parseExpression() (*Expression, error) {
 	f, err := p.parseField()
 	if err == nil {
-		return &Expression{Field: f}, nil
+		return &Expression{IsField: true, Field: f}, nil
 	}
 
 	c, err := p.parseConstant()
 	if err == nil {
-		return &Expression{Constant: c}, nil
+		return &Expression{IsField: false, Constant: c}, nil
 	}
 
 	return nil, errors.New("invalid syntax")
+}
+
+func (p *Parser) parseComparisonOperator() (string, error) {
+	switch {
+	case p.lexer.matchOperator("="):
+		if err := p.lexer.eatOperator("="); err != nil {
+			return "", err
+		}
+		return "=", nil
+	case p.lexer.matchOperator("<"):
+		if err := p.lexer.eatOperator("<"); err != nil {
+			return "", err
+		}
+		return "<", nil
+	case p.lexer.matchOperator(">"):
+		if err := p.lexer.eatOperator(">"); err != nil {
+			return "", err
+		}
+		return ">", nil
+	case p.lexer.matchOperator("<="):
+		if err := p.lexer.eatOperator("<="); err != nil {
+			return "", err
+		}
+		return "<=", nil
+	case p.lexer.matchOperator(">="):
+		if err := p.lexer.eatOperator(">="); err != nil {
+			return "", err
+		}
+		return ">=", nil
+	case p.lexer.matchOperator("!="):
+		if err := p.lexer.eatOperator("!="); err != nil {
+			return "", err
+		}
+		return "!=", nil
+	case p.lexer.matchOperator("<>"):
+		if err := p.lexer.eatOperator("<>"); err != nil {
+			return "", err
+		}
+		return "<>", nil
+	default:
+		return "", errors.New("invalid syntax")
+	}
 }
 
 func (p *Parser) parseTerm() (*Term, error) {
@@ -142,45 +265,9 @@ func (p *Parser) parseTerm() (*Term, error) {
 		return nil, err
 	}
 
-	var op string
-	switch {
-	case p.lexer.matchOperator("="):
-		if err = p.lexer.eatOperator("="); err != nil {
-			return nil, err
-		}
-		op = "="
-	case p.lexer.matchOperator("<"):
-		if err = p.lexer.eatOperator("<"); err != nil {
-			return nil, err
-		}
-		op = "<"
-	case p.lexer.matchOperator(">"):
-		if err = p.lexer.eatOperator(">"); err != nil {
-			return nil, err
-		}
-		op = ">"
-	case p.lexer.matchOperator("<="):
-		if err = p.lexer.eatOperator("<="); err != nil {
-			return nil, err
-		}
-		op = "<="
-	case p.lexer.matchOperator(">="):
-		if err = p.lexer.eatOperator(">="); err != nil {
-			return nil, err
-		}
-		op = ">="
-	case p.lexer.matchOperator("!="):
-		if err = p.lexer.eatOperator("!="); err != nil {
-			return nil, err
-		}
-		op = "!="
-	case p.lexer.matchOperator("<>"):
-		if err = p.lexer.eatOperator("<>"); err != nil {
-			return nil, err
-		}
-		op = "<>"
-	default:
-		return nil, errors.New("invalid syntax")
+	op, err := p.parseComparisonOperator()
+	if err != nil {
+		return nil, err
 	}
 
 	rhs, err := p.parseExpression()
@@ -238,21 +325,21 @@ func (p *Parser) parsePredicate() (*Predicate, error) {
 func (p *Parser) parseFieldList() (*FieldList, error) {
 	fields := make([]*Field, 0)
 
-	field, err := p.parseField()
+	f, err := p.parseField()
 	if err != nil {
 		return nil, err
 	}
-	fields = append(fields, field)
+	fields = append(fields, f)
 
 	if p.lexer.matchDelim(',') {
 		if err = p.lexer.eatDelim(','); err != nil {
 			return nil, err
 		}
-		selectList, err := p.parseFieldList() // TODO: Can this be done using a loop instead of recursion?
+		fl, err := p.parseFieldList() // TODO: Can this be done using a loop instead of recursion?
 		if err != nil {
 			return nil, err
 		}
-		fields = append(fields, selectList.Fields...)
+		fields = append(fields, fl.Fields...)
 	}
 
 	return &FieldList{Fields: fields}, nil
@@ -313,10 +400,396 @@ func (p *Parser) parseQuery() (*Query, error) {
 	}, nil
 }
 
-func (p *Parser) Parse() (*Query, error) {
-	if p.lexer.matchKeyword("SELECT") {
-		return p.parseQuery()
+func (p *Parser) parseConstList() (*ConstList, error) {
+	consts := make([]*Constant, 0)
+
+	c, err := p.parseConstant()
+	if err != nil {
+		return nil, err
+	}
+	consts = append(consts, c)
+
+	if p.lexer.matchDelim(',') {
+		if err = p.lexer.eatDelim(','); err != nil {
+			return nil, err
+		}
+		cl, err := p.parseConstList() // TODO: Can this be done using a loop instead of recursion?
+		if err != nil {
+			return nil, err
+		}
+		consts = append(consts, cl.Constants...)
 	}
 
-	return nil, errors.New("invalid syntax")
+	return &ConstList{Constants: consts}, nil
+}
+
+func (p *Parser) parseTypeDef() (*TypeDef, error) {
+	switch {
+	case p.lexer.matchKeyword("SMALLINT"):
+		if err := p.lexer.eatKeyword("SMALLINT"); err != nil {
+			return nil, err
+		}
+		return &TypeDef{Type: "SMALLINT"}, nil
+	case p.lexer.matchKeyword("INT"):
+		if err := p.lexer.eatKeyword("INT"); err != nil {
+			return nil, err
+		}
+		return &TypeDef{Type: "INT"}, nil
+	case p.lexer.matchKeyword("BIGINT"):
+		if err := p.lexer.eatKeyword("BIGINT"); err != nil {
+			return nil, err
+		}
+		return &TypeDef{Type: "BIGINT"}, nil
+	case p.lexer.matchKeyword("FLOAT"):
+		if err := p.lexer.eatKeyword("FLOAT"); err != nil {
+			return nil, err
+		}
+		return &TypeDef{Type: "FLOAT"}, nil
+	case p.lexer.matchKeyword("DOUBLE"):
+		if err := p.lexer.eatKeyword("DOUBLE"); err != nil {
+			return nil, err
+		}
+		return &TypeDef{Type: "DOUBLE"}, nil
+	case p.lexer.matchKeyword("CHAR") || p.lexer.matchKeyword("VARCHAR"):
+		var kw string
+		if p.lexer.matchKeyword("CHAR") {
+			kw = "CHAR"
+		} else {
+			kw = "VARCHAR"
+		}
+
+		if err := p.lexer.eatKeyword(kw); err != nil {
+			return nil, err
+		}
+
+		if !p.lexer.matchDelim('(') {
+			return nil, errors.New("invalid syntax")
+		}
+		if err := p.lexer.eatDelim('('); err != nil {
+			return nil, err
+		}
+
+		if !p.lexer.matchIntConstant() {
+			return nil, errors.New("invalid syntax")
+		}
+		size, err := p.lexer.eatIntConstant()
+		if err != nil {
+			return nil, err
+		}
+
+		if !p.lexer.matchDelim(')') {
+			return nil, errors.New("invalid syntax")
+		}
+		if err := p.lexer.eatDelim(')'); err != nil {
+			return nil, err
+		}
+
+		return &TypeDef{Type: kw, Size: size}, nil
+	default:
+		return nil, errors.New("invalid syntax")
+	}
+}
+
+func (p *Parser) parseFieldDef() (*FieldDef, error) {
+	if !p.lexer.matchIdentifier() {
+		return nil, errors.New("invalid syntax")
+	}
+	id, err := p.lexer.eatIdentifier()
+	if err != nil {
+		return nil, err
+	}
+
+	typeDef, err := p.parseTypeDef()
+	if err != nil {
+		return nil, err
+	}
+
+	return &FieldDef{
+		Name:    id,
+		TypeDef: typeDef,
+	}, nil
+}
+
+func (p *Parser) parseFieldDefs() (*FieldDefs, error) {
+	defs := make([]*FieldDef, 0)
+
+	d, err := p.parseFieldDef()
+	if err != nil {
+		return nil, err
+	}
+	defs = append(defs, d)
+
+	if p.lexer.matchDelim(',') {
+		if err = p.lexer.eatDelim(','); err != nil {
+			return nil, err
+		}
+		fds, err := p.parseFieldDefs() // TODO: Can this be done using a loop instead of recursion?
+		if err != nil {
+			return nil, err
+		}
+		defs = append(defs, fds.FieldDefs...)
+	}
+
+	return &FieldDefs{FieldDefs: defs}, nil
+}
+
+func (p *Parser) parseCreateTable() (*CreateTableData, error) {
+	if !p.lexer.matchKeyword("CREATE") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("CREATE"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchKeyword("TABLE") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("TABLE"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchIdentifier() {
+		return nil, errors.New("invalid syntax")
+	}
+	tableName, err := p.lexer.eatIdentifier()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim('(') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim('('); err != nil {
+		return nil, err
+	}
+
+	fieldDefs, err := p.parseFieldDefs()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim(')') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim(')'); err != nil {
+		return nil, err
+	}
+
+	return &CreateTableData{
+		TableName: tableName,
+		FieldDefs: fieldDefs,
+	}, nil
+}
+
+func (p *Parser) parseInsert() (*InsertData, error) {
+	if !p.lexer.matchKeyword("INSERT") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("INSERT"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchKeyword("INTO") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("INTO"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchIdentifier() {
+		return nil, errors.New("invalid syntax")
+	}
+	tableName, err := p.lexer.eatIdentifier()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim('(') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim('('); err != nil {
+		return nil, err
+	}
+
+	fieldList, err := p.parseFieldList()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim(')') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim(')'); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchKeyword("VALUES") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("VALUES"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim('(') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim('('); err != nil {
+		return nil, err
+	}
+
+	constList, err := p.parseConstList()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchDelim(')') {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatDelim(')'); err != nil {
+		return nil, err
+	}
+
+	return &InsertData{
+		TableName: tableName,
+		Fields:    fieldList,
+		Values:    constList,
+	}, nil
+}
+
+func (p *Parser) parseDelete() (*DeleteData, error) {
+	if !p.lexer.matchKeyword("DELETE") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("DELETE"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchKeyword("FROM") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("FROM"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchIdentifier() {
+		return nil, errors.New("invalid syntax")
+	}
+	tableName, err := p.lexer.eatIdentifier()
+	if err != nil {
+		return nil, err
+	}
+
+	var predicate *Predicate
+	if p.lexer.matchKeyword("WHERE") {
+		if err := p.lexer.eatKeyword("WHERE"); err != nil {
+			return nil, err
+		}
+		pred, err := p.parsePredicate()
+		if err != nil {
+			return nil, err
+		}
+		predicate = pred
+	}
+
+	return &DeleteData{
+		TableName: tableName,
+		Predicate: predicate,
+	}, nil
+}
+
+func (p *Parser) parseUpdate() (*UpdateData, error) {
+	if !p.lexer.matchKeyword("UPDATE") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("UPDATE"); err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchIdentifier() {
+		return nil, errors.New("invalid syntax")
+	}
+	tableName, err := p.lexer.eatIdentifier()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchKeyword("SET") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatKeyword("SET"); err != nil {
+		return nil, err
+	}
+
+	field, err := p.parseField()
+	if err != nil {
+		return nil, err
+	}
+
+	if !p.lexer.matchOperator("=") {
+		return nil, errors.New("invalid syntax")
+	}
+	if err := p.lexer.eatOperator("="); err != nil {
+		return nil, err
+	}
+
+	expression, err := p.parseExpression()
+	if err != nil {
+		return nil, err
+	}
+
+	var predicate *Predicate
+	if p.lexer.matchKeyword("WHERE") {
+		if err := p.lexer.eatKeyword("WHERE"); err != nil {
+			return nil, err
+		}
+		pred, err := p.parsePredicate()
+		if err != nil {
+			return nil, err
+		}
+		predicate = pred
+	}
+
+	return &UpdateData{
+		TableName:  tableName,
+		Field:      field,
+		Expression: expression,
+		Predicate:  predicate,
+	}, nil
+}
+
+func (p *Parser) Parse() (*ParseTree, error) {
+	switch {
+	case p.lexer.matchKeyword("SELECT"):
+		q, err := p.parseQuery()
+		if err != nil {
+			return nil, err
+		}
+		return &ParseTree{Type: SelectTree, Query: q}, nil
+	case p.lexer.matchKeyword("CREATE"):
+		data, err := p.parseCreateTable()
+		if err != nil {
+			return nil, err
+		}
+		return &ParseTree{Type: CreateTableTree, CreateTableData: data}, nil
+	case p.lexer.matchKeyword("INSERT"):
+		data, err := p.parseInsert()
+		if err != nil {
+			return nil, err
+		}
+		return &ParseTree{Type: InsertTree, InsertData: data}, nil
+	case p.lexer.matchKeyword("UPDATE"):
+		data, err := p.parseUpdate()
+		if err != nil {
+			return nil, err
+		}
+		return &ParseTree{Type: UpdateTree, UpdateData: data}, nil
+	case p.lexer.matchKeyword("DELETE"):
+		data, err := p.parseDelete()
+		if err != nil {
+			return nil, err
+		}
+		return &ParseTree{Type: DeleteTree, DeleteData: data}, nil
+	default:
+		return nil, errors.New("invalid syntax")
+	}
 }
--- compiler/parser/parser_test.go
+++ compiler/parser/parser_test.go
@@ -1,17 +1,29 @@
 package parser
 
 import (
+	"strconv"
 	"strings"
 	"testing"
 )
+
+// TODO: Implement tests for invalid SQL
 
 func TestParserSelectStar(t *testing.T) {
 	sql := "SELECT * FROM employees WHERE name = 'Mohamed'"
 	p := NewParser(sql)
 
-	q, err := p.Parse()
+	pt, err := p.Parse()
 	if err != nil {
 		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != SelectTree {
+		t.Fatalf("Expected SelectTree, got: %v", pt.Type)
+	}
+
+	q := pt.Query
+	if q == nil {
+		t.Fatalf("Expected a query, got: nil")
 	}
 
 	var b strings.Builder
@@ -50,9 +62,18 @@ func TestParserSelect(t *testing.T) {
 	sql := "SELECT name, age, dept FROM employees WHERE name = 'Mohamed'"
 	p := NewParser(sql)
 
-	q, err := p.Parse()
+	pt, err := p.Parse()
 	if err != nil {
 		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != SelectTree {
+		t.Fatalf("Expected SelectTree, got: %v", pt.Type)
+	}
+
+	q := pt.Query
+	if q == nil {
+		t.Fatalf("Expected a query, got: nil")
 	}
 
 	var b strings.Builder
@@ -84,5 +105,193 @@ func TestParserSelect(t *testing.T) {
 
 	if b.String() != sql {
 		t.Errorf("Expected 'SELECT * FROM employees WHERE name = 'Mohamed'', got: '%s'", b.String())
+	}
+}
+
+func TestParserCreateTable(t *testing.T) {
+	sql := "CREATE TABLE employees (name VARCHAR(32), age INT, dept CHAR(3))"
+	p := NewParser(sql)
+
+	pt, err := p.Parse()
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != CreateTableTree {
+		t.Fatalf("Expected CreateTableTree, got: %v", pt.Type)
+	}
+
+	data := pt.CreateTableData
+	if data == nil {
+		t.Fatalf("Expected a create table statement, got: nil")
+	}
+
+	var b strings.Builder
+
+	b.WriteString("CREATE TABLE ")
+	b.WriteString(data.TableName)
+	b.WriteString(" (")
+	for i, fieldDef := range data.FieldDefs.FieldDefs {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(fieldDef.Name)
+		b.WriteString(" ")
+		b.WriteString(fieldDef.TypeDef.Type)
+		if fieldDef.TypeDef.Size > 0 {
+			b.WriteString("(")
+			b.WriteString(strconv.Itoa(fieldDef.TypeDef.Size))
+			b.WriteString(")")
+		}
+	}
+	b.WriteString(")")
+
+	if b.String() != sql {
+		t.Errorf("Expected 'CREATE TABLE employees (name VARCHAR(50), age INT, dept VARCHAR(50))', got: '%s'", b.String())
+	}
+}
+
+func TestParserInsert(t *testing.T) {
+	sql := "INSERT INTO employees (name, age, dept) VALUES ('Mohamed', 30, 'HR')"
+	p := NewParser(sql)
+
+	pt, err := p.Parse()
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != InsertTree {
+		t.Fatalf("Expected InsertTree, got: %v", pt.Type)
+	}
+
+	data := pt.InsertData
+	if data == nil {
+		t.Fatalf("Expected a insert statement, got: nil")
+	}
+
+	var b strings.Builder
+
+	b.WriteString("INSERT INTO ")
+	b.WriteString(data.TableName)
+	b.WriteString(" (")
+	for i, field := range data.Fields.Fields {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(field.Name)
+	}
+	b.WriteString(") VALUES (")
+	for i, constant := range data.Values.Constants {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		switch constant.Type {
+		case StringToken:
+			b.WriteString("'")
+			b.WriteString(constant.StrVal)
+			b.WriteString("'")
+		case IntegerToken:
+			b.WriteString(strconv.Itoa(constant.IntVal))
+		case FloatToken:
+			b.WriteString(strconv.FormatFloat(constant.FloatVal, 'f', -1, 64))
+		default:
+			t.Fatalf("Unexpected value type: %T", constant.Type)
+		}
+	}
+	b.WriteString(")")
+
+	if b.String() != sql {
+		t.Errorf("Expected 'INSERT INTO employees (name, age, dept) VALUES ('Mohamed', 30, 'HR')', got: '%s'", b.String())
+	}
+}
+
+func TestParserDelete(t *testing.T) {
+	sql := "DELETE FROM employees WHERE name = 'Mohamed'"
+	p := NewParser(sql)
+
+	pt, err := p.Parse()
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != DeleteTree {
+		t.Fatalf("Expected DeleteTree, got: %v", pt.Type)
+	}
+
+	data := pt.DeleteData
+	if data == nil {
+		t.Fatalf("Expected a delete statement, got: nil")
+	}
+
+	var b strings.Builder
+
+	b.WriteString("DELETE FROM ")
+	b.WriteString(data.TableName)
+	b.WriteString(" WHERE ")
+	b.WriteString(data.Predicate.Term.Lhs.Field.Name)
+	b.WriteString(" ")
+	b.WriteString(data.Predicate.Term.Op)
+	b.WriteString(" ")
+	b.WriteString("'")
+	b.WriteString(data.Predicate.Term.Rhs.Constant.StrVal)
+	b.WriteString("'")
+
+	if b.String() != sql {
+		t.Errorf("Expected 'DELETE FROM employees WHERE name = 'Mohamed'', got: '%s'", b.String())
+	}
+}
+
+func TestParserUpdate(t *testing.T) {
+	sql := "UPDATE employees SET age = 31 WHERE name = 'Mohamed'"
+	p := NewParser(sql)
+
+	pt, err := p.Parse()
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if pt.Type != UpdateTree {
+		t.Fatalf("Expected UpdateTree, got: %v", pt.Type)
+	}
+
+	data := pt.UpdateData
+	if data == nil {
+		t.Fatalf("Expected a update statement, got: nil")
+	}
+
+	var b strings.Builder
+
+	b.WriteString("UPDATE ")
+	b.WriteString(data.TableName)
+	b.WriteString(" SET ")
+	b.WriteString(data.Field.Name)
+	b.WriteString(" = ")
+	if data.Expression.IsField {
+		b.WriteString(data.Expression.Field.Name)
+	} else {
+		switch data.Expression.Constant.Type {
+		case StringToken:
+			b.WriteString("'")
+			b.WriteString(data.Expression.Constant.StrVal)
+			b.WriteString("'")
+		case IntegerToken:
+			b.WriteString(strconv.Itoa(data.Expression.Constant.IntVal))
+		case FloatToken:
+			b.WriteString(strconv.FormatFloat(data.Expression.Constant.FloatVal, 'f', -1, 64))
+		default:
+			t.Fatalf("Unexpected value type: %T", data.Expression.Constant.Type)
+		}
+	}
+	b.WriteString(" WHERE ")
+	b.WriteString(data.Predicate.Term.Lhs.Field.Name)
+	b.WriteString(" ")
+	b.WriteString(data.Predicate.Term.Op)
+	b.WriteString(" ")
+	b.WriteString("'")
+	b.WriteString(data.Predicate.Term.Rhs.Constant.StrVal)
+	b.WriteString("'")
+
+	if b.String() != sql {
+		t.Errorf("Expected 'UPDATE employees SET age = 31 WHERE name = 'Mohamed'', got: '%s'", b.String())
 	}
 }
